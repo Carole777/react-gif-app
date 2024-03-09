12119ff168a8ff0ee0f65c5accddc75074bdd6ee
Carlos Pérez <66978636+Carole777@users.noreply.github.com>
2024-03-08 20:19:17 -0600

    Add files via upload

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,4 @@
-const api_key = `9eq74WvhIkMWILXXEcIfkAfqqutN9ozP`;
+const api_key = `hidden`;
 export const getGifs = async( category ) =>{
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${ category }&limit=10`;
   const resp = await fetch(url);
